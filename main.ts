1f8102b54283c99c090c91fabac3f66a97044342
cancelself <cancelself@gmail.com>
2023-10-11 13:40:14 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import { serve } from "./deps.ts";
 
 const port = 8000;
-const server = serve({ port });
 
 console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
 
-for await (const request of server) {
-    request.respond({ body: "Hello World\n" });
+async function transform(request: Request): Promise<Response> {
+    return new Response("Hello World\n");
 }
+
+serve(transform, { port });
