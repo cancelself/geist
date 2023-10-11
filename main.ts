43e1352b608d25cf55245d765b30f06a1d113aed
cancelself <cancelself@gmail.com>
2023-10-11 13:33:09 -0700

    add a helloworld server

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
 import { serve } from "./deps.ts";
+
+const port = 8000;
+const server = serve({ port });
+
+console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
+
+for await (const request of server) {
+    request.respond({ body: "Hello World\n" });
+}
